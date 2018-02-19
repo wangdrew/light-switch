fdd28d15254e58aaa8bdfc268a5f531f78592d57
Andrew Wang <wang.andrew11@gmail.com>
2018-02-18 18:50:28 -0800

    spaces not tabs

--- light-switch/wink_api.py
+++ light-switch/wink_api.py
@@ -12,7 +12,7 @@ class WinkApi(object):
         self.creds = creds
 
     def on_put(self, req, resp, device_type, device_id):
-	data = req.stream.read()
+    data = req.stream.read()
         wink_resp = self.update_device_state(device_type, device_id, data)
 
         resp.status = str(wink_resp.status_code) + ' ' + wink_resp.reason
@@ -32,11 +32,11 @@ class WinkApi(object):
         headers['Authorization'] = 'Bearer ' + self.creds.get_access_token()
         headers['Content-Type'] = 'application/json'
         resp = requests.put(url, data=data, headers=headers)
-	if resp.status_code == 401:
-		self.creds.refresh_tokens()
-		headers['Authorization'] = 'Bearer ' + self.creds.get_access_token()
-		resp = requests.put(url, data=data, headers=headers)
-	return resp
+    if resp.status_code == 401:
+        self.creds.refresh_tokens()
+        headers['Authorization'] = 'Bearer ' + self.creds.get_access_token()
+        resp = requests.put(url, data=data, headers=headers)
+    return resp
 
     def get_device(self, device_type, device_id):
         url = WINK_API_URL + device_type + '/' + device_id
@@ -44,52 +44,52 @@ class WinkApi(object):
         headers['Authorization'] = 'Bearer ' + self.creds.get_access_token()
         headers['Content-Type'] = 'application/json'
         resp = requests.get(url, headers=headers)
-	if resp.status_code == 401:
-		self.creds.refresh_tokens()
-		headers['Authorization'] = 'Bearer ' + self.creds.get_access_token()
-        	resp = requests.get(url, headers=headers)
-	return resp
+    if resp.status_code == 401:
+        self.creds.refresh_tokens()
+        headers['Authorization'] = 'Bearer ' + self.creds.get_access_token()
+            resp = requests.get(url, headers=headers)
+    return resp
 
 class WinkCredentials(object):
-	def __init__(self, path):
-		self.path = path
-		self.read_credentials()
-	
-	def get_access_token(self):
-		return self.wink_access_token
+    def __init__(self, path):
+        self.path = path
+        self.read_credentials()
+    
+    def get_access_token(self):
+        return self.wink_access_token
 
-	def refresh_tokens(self):
-		params = {
-			"client_id": self.wink_client_id,
-			"client_secret": self.wink_client_secret,
-			"grant_type": "refresh_token",
-			"refresh_token": self.wink_refresh_token
-		}
-		resp = requests.post(WINK_OAUTH_URL, params=params)
+    def refresh_tokens(self):
+        params = {
+            "client_id": self.wink_client_id,
+            "client_secret": self.wink_client_secret,
+            "grant_type": "refresh_token",
+            "refresh_token": self.wink_refresh_token
+        }
+        resp = requests.post(WINK_OAUTH_URL, params=params)
 
-		if resp.status_code >= 400:
-			raise RuntimeError("Error trying to refresh the access token. Data: " + resp.text)
-		
-		self.wink_access_token = resp.json()["data"]["access_token"]
-		self.wink_refresh_token = resp.json()["data"]["refresh_token"]
-		self.write_credentials()
+        if resp.status_code >= 400:
+            raise RuntimeError("Error trying to refresh the access token. Data: " + resp.text)
+        
+        self.wink_access_token = resp.json()["data"]["access_token"]
+        self.wink_refresh_token = resp.json()["data"]["refresh_token"]
+        self.write_credentials()
 
-	def read_credentials(self):
-		f = open(self.path, "r")
-		creds = json.loads(f.read())
-		f.close()
-		self.wink_client_id = creds["wink_client_id"]
-		self.wink_client_secret = creds["wink_client_secret"]
-		self.wink_access_token = creds["wink_access_token"]
-		self.wink_refresh_token = creds["wink_refresh_token"]
+    def read_credentials(self):
+        f = open(self.path, "r")
+        creds = json.loads(f.read())
+        f.close()
+        self.wink_client_id = creds["wink_client_id"]
+        self.wink_client_secret = creds["wink_client_secret"]
+        self.wink_access_token = creds["wink_access_token"]
+        self.wink_refresh_token = creds["wink_refresh_token"]
 
-	def write_credentials(self):
-		f = open(self.path, "w")
-		f.write(json.dumps({
-			"wink_client_id": self.wink_client_id,
-			"wink_client_secret": self.wink_client_secret,
-			"wink_access_token": self.wink_access_token,
-			"wink_refresh_token": self.wink_refresh_token
-		}))
-		f.close()	
+    def write_credentials(self):
+        f = open(self.path, "w")
+        f.write(json.dumps({
+            "wink_client_id": self.wink_client_id,
+            "wink_client_secret": self.wink_client_secret,
+            "wink_access_token": self.wink_access_token,
+            "wink_refresh_token": self.wink_refresh_token
+        }))
+        f.close()   
 
